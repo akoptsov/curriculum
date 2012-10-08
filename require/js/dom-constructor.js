5db0c2a910606351a48cd43438e857726165b9b4
Alexander Koptsov <sadsorceror@gmail.com>
2012-10-09 02:19:25 +0400

    Добавлено отображение данных модели в DOM

--- require/js/dom-constructor.js
+++ require/js/dom-constructor.js
@@ -1,4 +1,4 @@
-define(function(require, exports, module){
+﻿define(function(require, exports, module){
 	require('handlebars');
 	$ = require('jquery');
 	
@@ -7,9 +7,9 @@ define(function(require, exports, module){
 			console.error('Handlebars is missing from global scope!');
 		}
 		exports[name] = Handlebars.compile(template);
+		ready.check();
 	}
 	
-	/*TODO: USE ASYNC LOADED EVENT HERE OR IT WILL FUCK UP MISERABLY*/
 	require(['text!templates/day.html'], function(str){
 		addTemplate('day', str);
 	});
@@ -22,27 +22,40 @@ define(function(require, exports, module){
 		addTemplate('week', str);
 	});
 	
+	var ready = {
+		check: function (){
+			if(exports.week && exports.day && exports.lecture){
+				this.happened = true;
+				this.callback && this.callback();
+			} 
+		},
+		subscribe: function(callback){
+			this.callback = callback;
+			this.happened && this.callback();
+		}
+	};
+	
 	exports.init = function(model){
 		$(function(){
-			if(!model.length) {
-				return;
-			}
-			
-			var doc = $(this),
-				container = doc.find('.b-curriculum');
-			
-			if(container.length) {
-				for(var i = 0, length = model.length; i < length; i++){
-					var week = $(exports.week(model[i]));
-					for(var j = 0, l2 = week.length; j < l2; j++) {
-						var day = $(exports.day(week[j]));
-						day.data(week[j]);
-						week.append(day);
-					}
-					container.append(week);
+			ready.subscribe(function(){
+				if(!model.length) {
+					return;
 				}
-			}
-			
+				
+				var container = $('.b-curriculum');
+				
+				if(container.length) {
+					for(var i = 0, length = model.length; i < length; i++){
+						var week = $(exports.week(model[i]));
+						for(var j = 0, l2 = model[i].length; j < l2; j++) {
+							var day = $(exports.day(model[i][j]));
+							day.data(model[i][j]);
+							week.append(day);
+						}
+						container.append(week);
+					}
+				}
+			});	
 		});
 	}
 	
