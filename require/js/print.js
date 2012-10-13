18334f3b13542274767efb5f1847f9cd4c1e4ca1
Alexander Koptsov <sadsorceror@gmail.com>
2012-10-13 21:47:41 +0400

    Добавлена страница печати и просмотр расширенной информации при наведении мышкой (напильник required)

---
+++ require/js/print.js
@@ -0,0 +1,14 @@
+﻿define(function(require, exports, module){
+	require('jquery');
+	var models = require('./model-factory');
+	var storage = require('./storage');
+	var ui = require('./dom-constructor');
+	var data = storage.get('modeldata');
+	
+	data
+		? ui.init(new models.Model(data))
+		: $(function() { 
+			$('body').addClass('b-page_state_nodata'); 
+		})
+
+});
